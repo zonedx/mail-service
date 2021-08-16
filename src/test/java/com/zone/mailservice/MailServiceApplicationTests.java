b8ad0f1a7106bf556684a67ed6ae6d150b6fb983
zone <zonedx@163.com>
2021-08-16 22:50:01 +0800

    initial project

---
+++ src/test/java/com/zone/mailservice/MailServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zone.mailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("begin test");
+    }
+
+}
